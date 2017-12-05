78ece71b2b470712c8c72fa46883438fcefc4ccb
Josh Zucker <zuckerje@gmail.com>
2017-12-05 00:43:47 -0500

    day5

---
+++ 5/q5.exs
@@ -0,0 +1,43 @@
+# note slow AF
+
+defmodule Q5 do
+  Code.require_file "../utils.exs", __DIR__
+
+  def jump(array, position, steps) do
+    if position > length(array) - 1 do
+      steps
+    else
+      value = Enum.at(array, position)
+      jump(List.replace_at(array, position, value + 1), position + value, steps + 1)
+    end
+  end
+
+  def jump_p2(array, position, steps) do
+    if position > length(array) - 1 do
+      steps
+    else
+      value = Enum.at(array, position)
+      if value >= 3 do
+        jump_p2(List.replace_at(array, position, value - 1), position + value, steps + 1)
+      else
+        jump_p2(List.replace_at(array, position, value + 1), position + value, steps + 1)
+      end
+    end
+  end
+
+  def p1 do
+    Utils.file_to_int_array("input.txt")
+      |> jump(0, 0)
+      |> IO.inspect
+  end
+
+  def p2 do
+    Utils.file_to_int_array("input.txt")
+    |> jump_p2(0, 0)
+    |> IO.inspect
+  end
+end
+
+Q5.p1()
+Q5.p2()
+
--- utils.exs
+++ utils.exs
@@ -7,6 +7,13 @@ defmodule Utils do
       |> Enum.map(fn(x) -> String.to_integer(x) end)
   end
 
+  def file_to_int_array(filepath) do
+    File.read(filepath)
+      |> elem(1)
+      |> String.split("\n")
+      |> Enum.map(fn(item) -> String.to_integer(item) end)
+  end
+
   def file_to_2d_int_array(filepath) do
     File.read(filepath)
       |> elem(1)
